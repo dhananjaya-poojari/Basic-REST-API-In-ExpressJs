61f4971b7750b3ab5181f6a900886346d553fa40
Dhananjaya Poojari <DPoojari@magsw.com>
2021-02-12 15:29:19 +0530

    Port value changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
+const Port=process.env.Port||3000
 
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true,useUnifiedTopology: true});
 
@@ -17,4 +17,4 @@ const subscribeRouter=require('./routers/subscribers')
 app.use('/subscribers',subscribeRouter)
 
 
-app.listen(4000,()=>console.log('Server started'))
+app.listen(Port,()=>console.log('Server started'))
